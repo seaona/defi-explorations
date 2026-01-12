178ccb853e796d728b0f3f4857e540105c8839e3
seaona <mariona@gmx.es>
2026-01-12 08:14:39 +0100

    defi setup

---
+++ test/UniswapV2SwapAmounts.t.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {IERC20} from "../src/interfaces/IERC20.sol";
+import {IWETH} from "../src/interfaces/IWETH.sol";
+import {IUniswapV2Router02} from "../src/interfaces/IUniswapV2Router02.sol";
+
+contract UniswapV2SwapAmountsTest is Test {
+    // Mainnet addresses
+    address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address constant MKR = 0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2;
+    address constant UNISWAP_V2_ROUTER_02 = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+
+    IWETH private constant weth = IWETH(WETH);
+    IERC20 private constant dai = IERC20(DAI);
+    IERC20 private constant mkr = IERC20(MKR);
+
+    IUniswapV2Router02 private constant router = IUniswapV2Router02(UNISWAP_V2_ROUTER_02);
+
+    function test_getAmountsOut() public view {
+        address[] memory path = new address[](3);
+        path[0] = WETH;
+        path[1] = DAI;
+        path[2] = MKR;
+
+        uint256 amountIn = 1e18;
+        uint[] memory amounts = router.getAmountsOut(amountIn, path);
+
+        console2.log("WETH", amounts[0]);
+        console2.log("DAI", amounts[1]);
+        console2.log("MKR", amounts[2]);
+    }
+}

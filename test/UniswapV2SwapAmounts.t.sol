07cad25ae0b9b8ac3c2d3ea52501c5fccb510575
seaona <mariona@gmx.es>
2026-01-14 08:00:23 +0100

    getAmountsIn

--- test/UniswapV2SwapAmounts.t.sol
+++ test/UniswapV2SwapAmounts.t.sol
@@ -32,4 +32,19 @@ contract UniswapV2SwapAmountsTest is Test {
         console2.log("DAI", amounts[1]);
         console2.log("MKR", amounts[2]);
     }
+
+    function test_getAmountsIn() public view {
+        address[] memory path = new address[](3);
+        path[0] = WETH;
+        path[1] = DAI;
+        path[2] = MKR;
+
+        uint256 amountOut = 1e16;
+
+        uint256[] memory amounts = router.getAmountsIn(amountOut, path);
+
+        console2.log("WETH", amounts[0]);
+        console2.log("DAI", amounts[1]);
+        console2.log("MKR", amounts[2]);
+    }
 }
